3daf98d05b7513217d72f322db676d2c4758df69
守辰 <shouchen.zz@alibaba-inc.com>
2025-12-06 20:41:35 +0800

    add agents.kruise.io as prefix blacklist

--- pkg/servers/e2b/metadata.go
+++ pkg/servers/e2b/metadata.go
@@ -1,5 +1,7 @@
 package e2b
 
+import agentsv1alpha1 "github.com/openkruise/agents/api/v1alpha1"
+
 var (
-	BlackListPrefix = []string{InternalPrefix}
+	BlackListPrefix = []string{InternalPrefix, agentsv1alpha1.InternalPrefix}
 )
