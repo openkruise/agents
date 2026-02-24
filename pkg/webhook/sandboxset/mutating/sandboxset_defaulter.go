b982edbd4ae512865f5d77c69fbe54aaeea81db3
liheng <liheng.zms@alibaba-inc.com>
2026-02-24 20:37:46 +0800

    flag default sandboxset persistent contents
    Signed-off-by: liheng <liheng.zms@alibaba-inc.com>

--- pkg/webhook/sandboxset/mutating/sandboxset_defaulter.go
+++ pkg/webhook/sandboxset/mutating/sandboxset_defaulter.go
@@ -3,10 +3,12 @@ package mutating
 import (
 	"context"
 	"encoding/json"
+	"fmt"
 	"net/http"
 	"reflect"
+	"strings"
 
-	"github.com/openkruise/agents/pkg/utils/defaults"
+	admissionv1 "k8s.io/api/admission/v1"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/utils/ptr"
 	"sigs.k8s.io/controller-runtime/pkg/client"
@@ -15,12 +17,31 @@ import (
 	agentsv1alpha1 "github.com/openkruise/agents/api/v1alpha1"
 	"github.com/openkruise/agents/pkg/discovery"
 	"github.com/openkruise/agents/pkg/features"
+	"github.com/openkruise/agents/pkg/utils/defaults"
 	utilfeature "github.com/openkruise/agents/pkg/utils/feature"
 )
 
 var (
 	controllerKind = agentsv1alpha1.SchemeGroupVersion.WithKind("SandboxSet")
+
+	defaultPersistentContents []string
 )
+
+func SetDefaultPersistentContents(contents string) error {
+	if contents == "" {
+		return nil
+	}
+	persistentContents := strings.Split(contents, ",")
+	for _, content := range persistentContents {
+		if content != agentsv1alpha1.PersistentContentIp &&
+			content != agentsv1alpha1.PersistentContentFilesystem &&
+			content != agentsv1alpha1.PersistentContentMemory {
+			return fmt.Errorf("default-sandboxset-persistent-contents is invalid and only supports three contents: ip, memory, and filesystem")
+		}
+	}
+	defaultPersistentContents = persistentContents
+	return nil
+}
 
 type SandboxSetDefaulter struct {
 	Client  client.Client
@@ -49,6 +70,10 @@ func (h *SandboxSetDefaulter) Handle(_ context.Context, req admission.Request) a
 
 	clone := obj.DeepCopy()
 	setDefaultPodTemplate(obj.Spec.Template)
+
+	if req.Operation == admissionv1.Create && len(obj.Spec.PersistentContents) == 0 && len(defaultPersistentContents) > 0 {
+		obj.Spec.PersistentContents = defaultPersistentContents
+	}
 
 	// Apply defaulting logic to volume claim templates
 	setDefaultVolumeClaimTemplates(obj.Spec.VolumeClaimTemplates)
