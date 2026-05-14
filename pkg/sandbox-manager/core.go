79dab90d1ca71c105036b7a5f2487ffd12189dca
rakshaak29 <rakshaak29@gmail.com>
2026-05-14 22:26:21 +0530

    style(sandbox-manager): fix gofmt issue in core.go

--- pkg/sandbox-manager/core.go
+++ pkg/sandbox-manager/core.go
@@ -196,8 +196,6 @@ func (b *SandboxManagerBuilder) Build() (*SandboxManager, error) {
 type SandboxManager struct {
 	peersManager       peers.Peers
 	memberlistBindPort int
-
-<<<<<<< HEAD
 	infra infra.Infrastructure
 	proxy *proxy.Server
 
